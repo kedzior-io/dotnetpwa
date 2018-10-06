c61efd76d7bab2ff75862d5e0ec3b40434fefe00
Artur Kędzior <artur@kedzior.es>
2018-10-06 02:40:20 +0200

    added session

--- kedzior.io.dotnetpwa/Startup.cs
+++ kedzior.io.dotnetpwa/Startup.cs
@@ -57,8 +57,9 @@ namespace kedzior.io.dotnetpwa
             });
 
             services.AddSingleton(typeof(IHttpContextAccessor), typeof(HttpContextAccessor));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddSessionStateTempDataProvider();
             services.AddProgressiveWebApp();
+            services.AddSession();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,7 +81,7 @@ namespace kedzior.io.dotnetpwa
             app.UseCookiePolicy();
 
             app.UseAuthentication();
-
+            app.UseSession();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
